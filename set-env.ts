5ffa5fa99b55d1d04fd383de15a8d3cc503f8efc
Diego <d.perezmartinez@gmail.com>
2018-12-13 13:21:49 +0100

    Config script on build tasks and more elegant env variable replacement

--- set-env.ts
+++ set-env.ts
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const argv = require('yargs').argv;
 
-const environment = argv.environment ? `.argv.environment` : '';
+const environment = argv.environment ? `.${argv.environment}` : '';
 
 const targetPath = `./src/environments/environment${environment}.ts`;
 
@@ -9,10 +9,23 @@ fs.readFile(targetPath, 'utf8', function(readError, data) {
   if (readError) {
     return console.log(readError);
   }
-  const result = data
-    .replace(/##COSMIC_BUCKET##/g, process.env.COSMIC_BUCKET)
-    .replace(/##COSMIC_READ_KEY##/g, process.env.COSMIC_READ_KEY)
-    .replace(/##COSMIC_WRITE_KEY##/g, process.env.COSMIC_WRITE_KEY);
+  let result = data;
+
+  if (process.env.COSMIC_BUCKET) {
+    console.log('Updating COSMIC_BUCKET');
+
+    result = result.replace(/(bucket_slug:\s*')(.*)(',)/g, `$1${process.env.COSMIC_BUCKET}$3`);
+  }
+  if (process.env.COSMIC_READ_KEY) {
+    console.log('Updating COSMIC_READ_KEY');
+
+    result = result.replace(/(read_key:\s*')(.*)(',)/g, `$1${process.env.COSMIC_READ_KEY}$3`);
+  }
+  if (process.env.COSMIC_WRITE_KEY) {
+    console.log('Updating COSMIC_WRITE_KEY');
+
+    result = result.replace(/(write_key:\s*')(.*)(',)/g, `$1${process.env.COSMIC_WRITE_KEY}$3`);
+  }
 
   fs.writeFile(targetPath, result, 'utf8', function(writeError) {
     if (writeError) {
